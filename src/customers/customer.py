43e2cc99d8452afc810f426f469d62743c2b9ac2
Diego Martínez <diegoariel83@gmail.com>
2024-11-17 23:04:14 -0300

    Introduce Test containers

---
+++ src/customers/customer.py
@@ -0,0 +1,61 @@
+import os
+
+import psycopg
+
+
+def get_connection():
+    host = os.getenv("DB_HOST", "localhost")
+    port = os.getenv("DB_PORT", "5432")
+    username = os.getenv("DB_USERNAME", "test")
+    password = os.getenv("DB_PASSWORD", "test")
+    database = os.getenv("DB_NAME", "postgres")
+    return psycopg.connect(f"host={host} dbname={database} user={username} password={password} port={port}")
+
+class Customer:
+    def __init__(self, cust_id, name, email):
+        self.id = cust_id
+        self.name = name
+        self.email = email
+
+    def __str__(self):
+        return f"Customer({self.id}, {self.name}, {self.email})"    
+
+def create_table():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE customers (
+                    id serial PRIMARY KEY,
+                    name varchar not null,
+                    email varchar not null unique)
+                """)
+            conn.commit()
+
+def create_customer(name, email):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO customers (name, email) VALUES (%s, %s)", (name, email))
+            conn.commit()
+
+
+def get_all_customers() -> list[Customer]:
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM customers")
+            return [Customer(cid, name, email) for cid, name, email in cur]
+
+
+def get_customer_by_email(email) -> Customer:
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, name, email FROM customers WHERE email = %s", (email,))
+            (cid, name, email) = cur.fetchone()
+            return Customer(cid, name, email)
+
+
+def delete_all_customers():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM customers")
+            conn.commit()
